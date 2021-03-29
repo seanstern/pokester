8a7bece873c7373085a6cec95449f63b9b1fcd26
seanstern <sean.stern@outlook.com>
2021-03-29 13:56:05 -0400

    Server built react app

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,10 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
-
-app.get(
-    '/',
-    (req, res) => res.send(
-        '<html><head></head><body><h1>Hello World</h1></body></html>'
-    ),
-);
+app.use(express.static(path.join(__dirname, '../../client/build')));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
 
 app.listen(5000, () => console.log('server listening on port 5000'));
