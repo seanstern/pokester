8c04d6ad0bf014ee2bec4729252ff2a4b7f17511
seanstern <sean.stern@outlook.com>
2022-04-11 22:09:42 -0400

    Add PokerRoomSchema tests
    Add tests for helper functions, virtuals, and middleware.
    Update jest.config to clearMocks between tests so that
    develpers need not write beforeEach when using mocks,
    spies.

--- server/jest.config.js
+++ server/jest.config.js
@@ -8,4 +8,5 @@ module.exports = {
     "<rootDir>/.*__fixtures__",
     "<rootDir>/.*\\.fixture\\.ts$",
   ],
+  clearMocks: true,
 };
--- server/src/models/PokerRoom/PokerRoomSchema.test.ts
+++ server/src/models/PokerRoom/PokerRoomSchema.test.ts
@@ -1,14 +1,87 @@
 import { model } from "mongoose";
-import { asJestCasesTable } from "../../__fixtures__/TestCaseUtils";
-import * as TableFixtures from "../../__fixtures__/poker-engine/Table.fixture";
 import { serialize } from "../../serializers/TableSerializer";
-import PokerRoomSchema from "./PokerRoomSchema";
+import * as TableFixtures from "../../__fixtures__/poker-engine/Table.fixture";
+import { asJestCasesTable } from "../../__fixtures__/TestCaseUtils";
+// import * in order to jest.spyOn below
+import * as PokerRoomSchemaModule from "./PokerRoomSchema";
 
-const TestPokerRoomModel = model("Test Poker Room", PokerRoomSchema);
+const TestPokerRoomModel = model<
+  PokerRoomSchemaModule.SerializedPokerRoomDoc,
+  PokerRoomSchemaModule.SerializedPokerRoomModel
+>("Test Poker Room", PokerRoomSchemaModule.default);
 
 const tableCases = asJestCasesTable(TableFixtures);
 
-describe("constructor properly sets document fields with", () => {
+describe("getPlayerIds returns playerIds when given", () => {
+  test.each(tableCases)("$description", ({ create }) => {
+    const table = create();
+    expect(PokerRoomSchemaModule.getPlayerIds(table)).toStrictEqual(
+      table.players.filter((p) => p !== null).map((p) => p!.id)
+    );
+  });
+});
+
+describe("virtuals", () => {
+  describe("virtualTableSetter sets this.playerIds, this.serializedTable, this.deserializedTable when given", () => {
+    test.each(tableCases)("$description", ({ create }) => {
+      const table = create();
+      const obj: ThisParameterType<
+        typeof PokerRoomSchemaModule.virtualTableSetter
+      > = {};
+      PokerRoomSchemaModule.virtualTableSetter.call(obj, table);
+
+      const { deserializedTable, ...rest } = obj;
+
+      expect({ ...rest }).toStrictEqual({
+        playerIds: PokerRoomSchemaModule.getPlayerIds(table),
+        serializedTable: serialize(table),
+      });
+      expect(deserializedTable).toBe(table);
+    });
+  });
+
+  test("virtualTableGetter returns this.deserializedTable", () => {
+    const table = TableFixtures.emptyTable.create();
+    const obj = { deserializedTable: table };
+    expect(PokerRoomSchemaModule.virtualTableGetter.call(obj)).toBe(table);
+  });
+});
+
+describe("middleware", () => {
+  test("postInitDeserializeTable calls virtualTableSetter", () => {
+    const virtualTableSetterSpy = jest.spyOn(
+      PokerRoomSchemaModule,
+      "virtualTableSetter"
+    );
+    const table = TableFixtures.emptyTable.create();
+    const obj: ThisParameterType<
+      typeof PokerRoomSchemaModule.postInitDeserializeTable
+    > = { serializedTable: serialize(table) };
+
+    PokerRoomSchemaModule.postInitDeserializeTable.call(obj);
+
+    expect(virtualTableSetterSpy).toHaveBeenCalledTimes(1);
+    expect(virtualTableSetterSpy).toHaveBeenCalledWith(table);
+  });
+
+  test("preValidateSerializeTable calls virtualTableSetter", () => {
+    const virtualTableSetterSpy = jest.spyOn(
+      PokerRoomSchemaModule,
+      "virtualTableSetter"
+    );
+    const table = TableFixtures.emptyTable.create();
+    const obj: ThisParameterType<
+      typeof PokerRoomSchemaModule.preValidateSerializeTable
+    > = { deserializedTable: table };
+
+    PokerRoomSchemaModule.preValidateSerializeTable.call(obj);
+
+    expect(virtualTableSetterSpy).toHaveBeenCalledTimes(1);
+    expect(virtualTableSetterSpy).toHaveBeenCalledWith(table);
+  });
+});
+
+describe("constructor properly sets document fields when given", () => {
   test.each(tableCases)("$description", ({ create }) => {
     const constructorArg = {
       creatorId: "le chiffre",
@@ -18,14 +91,16 @@ describe("constructor properly sets document fields with", () => {
     const {
       name,
       creatorId,
+      table,
       serializedTable,
       playerIds,
     } = new TestPokerRoomModel(constructorArg);
     expect(name).toStrictEqual(constructorArg.name);
     expect(creatorId).toStrictEqual(constructorArg.creatorId);
+    expect(table).toStrictEqual(table);
     expect(serializedTable).toStrictEqual(serialize(constructorArg.table));
     expect(playerIds).toStrictEqual(
-      constructorArg.table.players.filter((p) => p !== null).map((p) => p!.id)
+      PokerRoomSchemaModule.getPlayerIds(constructorArg.table)
     );
   });
 });
