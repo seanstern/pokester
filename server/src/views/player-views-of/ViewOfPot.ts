6407523db02b11a5d151ad1f1bfecc5a53eef530
seanstern <sean.stern@outlook.com>
2022-05-15 14:52:30 -0400

    Add ViewOfPot

---
+++ server/src/views/player-views-of/ViewOfPot.ts
@@ -0,0 +1,34 @@
+import { Pot as PokerEnginePot } from "@chevtek/poker-engine";
+import { Pot as CommonAPIPot } from "common-api";
+import viewOfPlayer from "./ViewOfPlayer";
+
+/**
+ * Given a poker-engine (i.e. server-side runtime) representation of a Pot,
+ * returns a common-api (i.e. serializable, server-to-client) representaiton
+ * of a Pot from the perspective of a player.
+ * @param viewerId the id of the player requesting the view
+ * @param p a poker-engine (i.e. server-side runtime) representatio of a Pot
+ * @returns a common-api (i.e. serializable, server-to-client) representation
+ *   of a Pot from the perspective of a player
+ */
+const viewOfPot = (viewerId: string, p: PokerEnginePot): CommonAPIPot => {
+  const {
+    amount,
+    eligiblePlayers: pokerEngineEligiblePlayers,
+    winners: pokerEngineWinners,
+  } = p;
+  return {
+    amount,
+    eligiblePlayers: pokerEngineEligiblePlayers.map(
+      (pokerEngineEligiblePlayer) =>
+        viewOfPlayer(viewerId, pokerEngineEligiblePlayer)
+    ),
+    winners: pokerEngineWinners
+      ? pokerEngineWinners.map((pokerEngineWinner) =>
+          viewOfPlayer(viewerId, pokerEngineWinner)
+        )
+      : undefined,
+  };
+};
+
+export default viewOfPot;
