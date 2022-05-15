8ef0b1115de40657862b1e633fa72c69623e8725
seanstern <sean.stern@outlook.com>
2022-05-15 14:52:40 -0400

    Add ViewOfTable

---
+++ server/src/views/player-views-of/ViewOfTable.ts
@@ -0,0 +1,56 @@
+import { Table as PokerEngineTable } from "@chevtek/poker-engine";
+import { Table as CommonAPITable } from "common-api";
+import viewOfCard from "./ViewOfCard";
+import viewOfPlayer from "./ViewOfPlayer";
+import viewOfPot from "./ViewOfPot";
+
+export const playerView = (
+  viewerId: string,
+  t: PokerEngineTable
+): CommonAPITable => {
+  const {
+    bigBlindPosition,
+    currentBet,
+    currentPosition,
+    currentRound,
+    dealerPosition,
+    handNumber,
+    smallBlindPosition,
+    buyIn,
+    smallBlind,
+    bigBlind,
+    communityCards: pokerEngineCommunityCards,
+    players: pokerEnginePlayers,
+    pots: pokerEnginePots,
+    winners: pokerEngineWinners,
+  } = t;
+
+  return {
+    bigBlindPosition,
+    currentBet,
+    currentPosition,
+    currentRound,
+    dealerPosition,
+    handNumber,
+    smallBlindPosition,
+    buyIn,
+    smallBlind,
+    bigBlind,
+    communityCards: pokerEngineCommunityCards.map((pokerEngineCommunityCard) =>
+      viewOfCard(pokerEngineCommunityCard)
+    ),
+    players: pokerEnginePlayers.map((pokerEnginePlayer) =>
+      pokerEnginePlayer === null
+        ? null
+        : viewOfPlayer(viewerId, pokerEnginePlayer)
+    ),
+    pots: pokerEnginePots.map((pokerEnginePot) =>
+      viewOfPot(viewerId, pokerEnginePot)
+    ),
+    winners: pokerEngineWinners
+      ? pokerEngineWinners.map((pokerEngineWinner) =>
+          viewOfPlayer(viewerId, pokerEngineWinner)
+        )
+      : undefined,
+  };
+};
