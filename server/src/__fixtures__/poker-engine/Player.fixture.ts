f5f46ab436ad3d0a615f0db9f2e46c720e5a3c4c
seanstern <sean.stern@outlook.com>
2022-05-22 12:38:00 -0400

    Update Player.fixture to throw when Player absent
    Because Player.fixtures depend on Table.fixtures and Table.fixtures
    could potentially be changed by a developer *independent of*
    Player.fixtures, Player.fixtures should not assume field(s) containing
    players (e.g. lastActor, currentActor, etc.) are necessarily defined;
    instead Player.fixture should check for presence and throw Error in
    exceptional cases.

--- server/src/__fixtures__/poker-engine/Player.fixture.ts
+++ server/src/__fixtures__/poker-engine/Player.fixture.ts
@@ -1,6 +1,6 @@
 import { Player } from "@chevtek/poker-engine";
 import { Fixture } from "../TypeUtils";
-import { playersSeated, preFlop, completeRound } from "./Table.fixture";
+import { playersSeated, preFlop, completeRound, turn } from "./Table.fixture";
 
 export const playerSeated: Fixture<Player> = {
   description: "Player seated at table, pre-game",
@@ -12,7 +12,10 @@ export const priorActorPostDealPreFlop: Fixture<Player> = {
     "Player immediately priort to current actor, immeidately after cards dealt",
   create: () => {
     const { lastActor } = preFlop.create();
-    return lastActor!;
+    if (!lastActor) {
+      throw new Error("No lastActor in preFlop Table.fixture");
+    }
+    return lastActor;
   },
 };
 
@@ -20,7 +23,23 @@ export const currentActorPostDealPreFlop: Fixture<Player> = {
   description: "Player who is current actor, immediately after cards dealt",
   create: () => {
     const { currentActor } = preFlop.create();
+    if (!currentActor) {
+      throw new Error("No currentActor in preFlop Table.fixture");
+    }
     return currentActor!;
+  },
+};
+
+export const foledPlayer: Fixture<Player> = {
+  description:
+    "Player who has folded during flop, immediately at start of turn",
+  create: () => {
+    const { players } = turn.create();
+    const foldedPlayer = players.find((player) => player?.folded === true);
+    if (!foldedPlayer) {
+      throw new Error("No foldedPlayer in turn Table.fixture");
+    }
+    return foldedPlayer;
   },
 };
 
@@ -28,6 +47,14 @@ export const winner: Fixture<Player> = {
   description: "Player who is winner, immediately after completion of round",
   create: () => {
     const { winners } = completeRound.create();
-    return winners![0];
+    if (!winners) {
+      throw new Error("No winners array in completeRound Table.fixture");
+    }
+    if (winners.length < 1) {
+      throw new Error(
+        "No elements in winners array in completeRound Table.fixture"
+      );
+    }
+    return winners[0];
   },
 };
