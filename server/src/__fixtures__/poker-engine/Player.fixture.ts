ddc686c814321ec9d51f6217ed083c60cca0a724
seanstern <sean.stern@outlook.com>
2022-05-22 12:06:46 -0400

    Update Player.fixture descriptions
    Because "last actor" is potentially ambiguous to testers, changed
    description to "prior to current actor"

--- server/src/__fixtures__/poker-engine/Player.fixture.ts
+++ server/src/__fixtures__/poker-engine/Player.fixture.ts
@@ -7,8 +7,9 @@ export const playerSeated: Fixture<Player> = {
   create: () => playersSeated.create().players[0]!,
 };
 
-export const lastActorPostDealPreFlop: Fixture<Player> = {
-  description: "Player who is last actor immeidately after cards dealt",
+export const priorActorPostDealPreFlop: Fixture<Player> = {
+  description:
+    "Player immediately priort to current actor, immeidately after cards dealt",
   create: () => {
     const { lastActor } = preFlop.create();
     return lastActor!;
@@ -16,7 +17,7 @@ export const lastActorPostDealPreFlop: Fixture<Player> = {
 };
 
 export const currentActorPostDealPreFlop: Fixture<Player> = {
-  description: "Player who is current actor immediately after cards dealt",
+  description: "Player who is current actor, immediately after cards dealt",
   create: () => {
     const { currentActor } = preFlop.create();
     return currentActor!;
@@ -24,7 +25,7 @@ export const currentActorPostDealPreFlop: Fixture<Player> = {
 };
 
 export const winner: Fixture<Player> = {
-  description: "Player who is winner immediately after completion of round",
+  description: "Player who is winner, immediately after completion of round",
   create: () => {
     const { winners } = completeRound.create();
     return winners![0];
