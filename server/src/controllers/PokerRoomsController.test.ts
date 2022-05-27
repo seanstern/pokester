2ad36b290a8adc43f53979dd397f04c574c9dfef
seanstern <sean.stern@outlook.com>
2022-05-27 16:23:19 -0400

    Update PokerRoomsController and test
    To support get endpoint

--- server/src/controllers/PokerRoomsController.test.ts
+++ server/src/controllers/PokerRoomsController.test.ts
@@ -1,6 +1,12 @@
 import { getMockReq, getMockRes } from "@jest-mock/express";
-import { create, CreateReqBody } from "./PokerRoomsController";
+import {
+  create,
+  CreateReqBody,
+  get,
+  GetReqParams,
+} from "./PokerRoomsController";
 import PokerRoom from "../models/PokerRoom";
+import { flop } from "../__fixtures__/poker-engine/Table.fixture";
 
 describe("create", () => {
   test("succeeds", async () => {
@@ -29,5 +35,162 @@ describe("create", () => {
     expect(res.status).toHaveBeenCalledWith(201);
     expect(res.json).toHaveBeenCalledTimes(1);
     expect(res.json).toHaveBeenCalledWith(expect.any(String));
+  });
+});
+
+describe("get", () => {
+  test("succeeds", async () => {
+    const params: GetReqParams = {
+      id: "roomId",
+    };
+    const req = getMockReq<Parameters<typeof get>[0]>({
+      sessionId: "sessionId",
+      params,
+    });
+    const { res, next } = getMockRes<Parameters<typeof get>[1]>();
+
+    const findOneMock = jest.spyOn(PokerRoom, "findOne").mockReturnValue({
+      exec: jest.fn().mockResolvedValue({ table: flop.create() }),
+    } as any);
+
+    await get(req, res, next);
+
+    expect(findOneMock).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledTimes(1);
+    expect((res.json as jest.Mock).mock.lastCall).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "bigBlind": 10,
+          "bigBlindPosition": 0,
+          "buyIn": 1000,
+          "communityCards": Array [
+            Object {
+              "color": "#000000",
+              "rank": "T",
+              "suit": "s",
+              "suitChar": "♠",
+            },
+            Object {
+              "color": "#000000",
+              "rank": "J",
+              "suit": "s",
+              "suitChar": "♠",
+            },
+            Object {
+              "color": "#000000",
+              "rank": "Q",
+              "suit": "s",
+              "suitChar": "♠",
+            },
+          ],
+          "currentBet": undefined,
+          "currentPosition": 7,
+          "currentRound": "flop",
+          "dealerPosition": 3,
+          "handNumber": 1,
+          "players": Array [
+            Object {
+              "bet": 0,
+              "folded": false,
+              "holeCards": undefined,
+              "id": "Jane",
+              "isSelf": false,
+              "left": false,
+              "legalActions": undefined,
+              "stackSize": 2990,
+            },
+            Object {
+              "bet": 0,
+              "folded": false,
+              "holeCards": undefined,
+              "id": "Juan",
+              "isSelf": false,
+              "left": false,
+              "legalActions": undefined,
+              "stackSize": 9189,
+            },
+            null,
+            Object {
+              "bet": 0,
+              "folded": false,
+              "holeCards": undefined,
+              "id": "John",
+              "isSelf": false,
+              "left": false,
+              "legalActions": undefined,
+              "stackSize": 1990,
+            },
+            null,
+            null,
+            null,
+            Object {
+              "bet": 0,
+              "folded": false,
+              "holeCards": undefined,
+              "id": "Jay",
+              "isSelf": false,
+              "left": false,
+              "legalActions": undefined,
+              "stackSize": 31405,
+            },
+            null,
+            null,
+          ],
+          "pots": Array [
+            Object {
+              "amount": 40,
+              "eligiblePlayers": Array [
+                Object {
+                  "bet": 0,
+                  "folded": false,
+                  "holeCards": undefined,
+                  "id": "Jane",
+                  "isSelf": false,
+                  "left": false,
+                  "legalActions": undefined,
+                  "stackSize": 2990,
+                },
+                Object {
+                  "bet": 0,
+                  "folded": false,
+                  "holeCards": undefined,
+                  "id": "Juan",
+                  "isSelf": false,
+                  "left": false,
+                  "legalActions": undefined,
+                  "stackSize": 9189,
+                },
+                Object {
+                  "bet": 0,
+                  "folded": false,
+                  "holeCards": undefined,
+                  "id": "John",
+                  "isSelf": false,
+                  "left": false,
+                  "legalActions": undefined,
+                  "stackSize": 1990,
+                },
+                Object {
+                  "bet": 0,
+                  "folded": false,
+                  "holeCards": undefined,
+                  "id": "Jay",
+                  "isSelf": false,
+                  "left": false,
+                  "legalActions": undefined,
+                  "stackSize": 31405,
+                },
+              ],
+              "winners": undefined,
+            },
+          ],
+          "smallBlind": 5,
+          "smallBlindPosition": 7,
+          "winners": undefined,
+        },
+      ]
+    `);
   });
 });
--- server/src/controllers/PokerRoomsController.ts
+++ server/src/controllers/PokerRoomsController.ts
@@ -1,6 +1,8 @@
 import { RequestHandler } from "express";
 import { Table } from "@chevtek/poker-engine";
+import { Table as CommonAPITable } from "common-api";
 import PokerRoom from "../models/PokerRoom";
+import viewOfTable from "../views/player-views-of/ViewOfTable";
 
 export type CreateReqBody = {
   name: string;
@@ -36,5 +38,36 @@ export const create: RequestHandler<
   } catch (err) {
     next(err);
     return;
+  }
+};
+
+export type GetReqParams = {
+  id: string;
+};
+export const get: RequestHandler<
+  GetReqParams,
+  CommonAPITable,
+  never,
+  never,
+  never
+> = async (req, res, next) => {
+  try {
+    const {
+      sessionID,
+      params: { id: roomId },
+    } = req;
+
+    const pr = await PokerRoom.findOne({
+      _id: roomId,
+      playerIds: sessionID,
+    }).exec();
+    if (!pr) {
+      throw new Error("PokerRoom does not exist");
+    }
+
+    res.status(200).json(viewOfTable(sessionID, pr.table));
+    return;
+  } catch (err) {
+    next(err);
   }
 };
