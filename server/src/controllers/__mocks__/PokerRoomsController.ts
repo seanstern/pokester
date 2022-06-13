f3b0b56c243e54ebf8b6561e37a8ac6554157d20
seanstern <sean.stern@outlook.com>
2022-06-12 21:34:50 -0400

    Add PokerRoomsRouter.test

---
+++ server/src/controllers/__mocks__/PokerRoomsController.ts
@@ -0,0 +1,9 @@
+import { RequestHandler } from "express";
+
+const emptyResponse: RequestHandler = (req, res) => res.end();
+
+export const create: RequestHandler = jest.fn(emptyResponse);
+
+export const get: RequestHandler = jest.fn(emptyResponse);
+
+export const act: RequestHandler = jest.fn(emptyResponse);
---
+++ server/src/routers/PokerRoomsRouter.test.ts
@@ -0,0 +1,40 @@
+import PokerRoomsRouter from "./PokerRoomsRouter";
+import { create, get, act } from "../controllers/PokerRoomsController";
+import request from "supertest";
+
+jest.mock("../controllers/PokerRoomsController");
+
+describe("'/' route", () => {
+  test("via POST calls PokerRoomsController.create", async () => {
+    await request(PokerRoomsRouter).post("/");
+    expect(create).toHaveBeenCalledTimes(1);
+  });
+});
+
+describe("'/:roomId' route", () => {
+  test("via GET calls PokerRoomsController.get", async () => {
+    const roomId = "someRoomId";
+    await request(PokerRoomsRouter).get(`/${roomId}`);
+    expect(get).toHaveBeenCalledTimes(1);
+    expect(get).toHaveBeenLastCalledWith(
+      expect.objectContaining({
+        params: { roomId },
+      }),
+      expect.anything(),
+      expect.anything()
+    );
+  });
+
+  test("via PATCH calls PokerRoomsController.act", async () => {
+    const roomId = "someRoomId";
+    await request(PokerRoomsRouter).patch(`/${roomId}`);
+    expect(act).toHaveBeenCalledTimes(1);
+    expect(act).toHaveBeenLastCalledWith(
+      expect.objectContaining({
+        params: { roomId },
+      }),
+      expect.anything(),
+      expect.anything()
+    );
+  });
+});
