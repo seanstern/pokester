d5bfc6085f81e660e6a4d6bae98ac0c813f4d137
Sean Stern <sean.stern@outlook.com>
2022-06-12 23:57:54 -0400

    Remove tic-tac-toe from server (#14)
    * Remove tictactoe routes
    
    * Remove tictactoe GameEngine
    
    * Remove tictactoe models

--- server/src/models/ITimestampedDocument.d.ts
+++
@@ -1,8 +0,0 @@
-import { Document } from "mongoose";
-
-interface ITimestampedDocument extends Document {
-  createdAt: Readonly<Date>;
-  updatedAt: Readonly<Date>;
-}
-
-export default ITimestampedDocument;
--- server/src/routes/apiRoutes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import gamesRoutes from "./gamesRoutes";
-
-const apiRoutes = Router();
-apiRoutes.use("/games", gamesRoutes);
-
-export default apiRoutes;
