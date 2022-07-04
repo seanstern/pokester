aee308c2031c555b9553bcacff0183e2f55793bb
seanstern <sean.stern@outlook.com>
2022-07-03 23:42:09 -0400

    Update common-api GetAll ResBody, ReqQuery
    To utilize canSit, isSeated

--- common-api/src/routes/poker-rooms/GetAll.ts
+++ common-api/src/routes/poker-rooms/GetAll.ts
@@ -1,10 +1,13 @@
 export type ResBody = {
   id: string;
   name: string;
+  canSit: boolean;
+  isSeated: boolean;
 }[];
 
 export type ReqQuery = {
   name?: string;
   creatorId?: string;
-  openSeat?: "true" | "false";
+  canSit?: "true" | "false";
+  isSeated?: "true" | "false";
 };
