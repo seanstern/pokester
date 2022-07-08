97067bd59a1ee522166dbf5fb997cdeebad42e10
seanstern <sean.stern@outlook.com>
2022-07-07 20:30:39 -0400

    Update server/poker-engine/Utils.test
    Also required re-writing fixtures which causes snapshot regen.

--- server/src/poker-engine/Utils.test.ts
+++ server/src/poker-engine/Utils.test.ts
@@ -1,34 +1,41 @@
 import {
   onlyDealerSeated,
   smallBlindAmongSeatedPlayers,
-  dealerAtTurn,
+  dealerAtFlop,
   dealerAmongSeatedPlayers,
-  dealerAfterFirstRound,
+  dealerStoodUpAtEndOfRound,
+  nonDealerPlayerWithStoodUpDealerAtEndOfRound,
 } from "../__fixtures__/poker-engine/Player.fixture";
 import { canDealCards } from "./Utils";
 
 describe("canDealCards", () => {
   describe("returns false", () => {
-    test("when only one player", () => {
+    test("when player is dealer but only one player", () => {
       expect(canDealCards(onlyDealerSeated.create())).toBe(false);
     });
 
-    test("when player isn't dealer", () => {
+    test("when player isn't dealer and dealer hasn't left", () => {
       expect(canDealCards(smallBlindAmongSeatedPlayers.create())).toBe(false);
     });
 
-    test("when round is active", () => {
-      expect(canDealCards(dealerAtTurn.create())).toBe(false);
+    test("when player is dealer and round is active", () => {
+      expect(canDealCards(dealerAtFlop.create())).toBe(false);
+    });
+
+    test("when player is dealer but has stood up", () => {
+      expect(canDealCards(dealerStoodUpAtEndOfRound.create())).toBe(false);
     });
   });
 
   describe("returns true", () => {
-    test("when dealer at table with players pre-game", () => {
+    test("when player is dealer at table with players pre-game", () => {
       expect(canDealCards(dealerAmongSeatedPlayers.create())).toBe(true);
     });
 
-    test("when dealer at table with players between rounds", () => {
-      expect(canDealCards(dealerAfterFirstRound.create())).toBe(true);
+    test("when player is non-dealer at end of round and dealer has already stood up", () => {
+      expect(
+        canDealCards(nonDealerPlayerWithStoodUpDealerAtEndOfRound.create())
+      ).toBe(true);
     });
   });
 });
