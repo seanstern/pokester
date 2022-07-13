77f30b4cf6e0bfc652fe3e17d3e7fb359cc21566
seanstern <sean.stern@outlook.com>
2022-07-12 23:34:23 -0400

    Update App to consume RoomsList

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,8 +8,9 @@ import {
   Link,
 } from "react-router-dom";
 import Room from "./Room";
-import Rooms from "./Rooms";
+import RoomsList from "./components/rooms-list";
 import CreateRoom from "./CreateRoom";
+import Container from "@mui/material/Container";
 
 const qc = new QueryClient({
   defaultOptions: { queries: { refetchOnWindowFocus: false } },
@@ -18,23 +19,25 @@ const qc = new QueryClient({
 const App: FC = () => (
   <QueryClientProvider client={qc}>
     <Router>
-      <Link to="/create">Create a Room</Link>
-      {" or "}
-      <Link to="/rooms">Browse Rooms</Link>{" "}
-      <Link to={"/rooms?canSit=true"}>(To Join)</Link>{" "}
-      <Link to={"/rooms?isSeated=true"}>(You're Already In)</Link>
-      <Switch>
-        <Route strict path="/rooms/:roomId">
-          <Room />
-        </Route>
-        <Route strict exact path="/rooms">
-          <Rooms />
-        </Route>
-        <Route strict path="/create">
-          <CreateRoom />
-        </Route>
-        <Redirect to="/rooms" />
-      </Switch>
+      <Container>
+        <Link to="/create">Create a Room</Link>
+        {" or "}
+        <Link to="/rooms">Browse Rooms</Link>{" "}
+        <Link to={"/rooms?canSit=true"}>(To Join)</Link>{" "}
+        <Link to={"/rooms?isSeated=true"}>(You're Already In)</Link>
+        <Switch>
+          <Route strict path="/rooms/:roomId">
+            <Room />
+          </Route>
+          <Route strict exact path="/rooms">
+            <RoomsList />
+          </Route>
+          <Route strict path="/create">
+            <CreateRoom />
+          </Route>
+          <Redirect to="/rooms" />
+        </Switch>
+      </Container>
     </Router>
   </QueryClientProvider>
 );
