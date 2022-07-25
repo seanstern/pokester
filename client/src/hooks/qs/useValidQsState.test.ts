b5f16603721c1e4c52ca3bae3e411cf2aad8b763
seanstern <sean.stern@outlook.com>
2022-07-25 13:32:28 -0400

    Update useValidQsState.test
    To allow tests to pass with no meaningful test

--- client/src/hooks/qs/useValidQsState.test.ts
+++ client/src/hooks/qs/useValidQsState.test.ts
@@ -5,4 +5,5 @@ describe("useValidQsState", () => {
   // https://kentcdodds.com/blog/how-to-test-custom-react-hooks
   // TL;DR this hook will be tested in real world example usage
   // via consuming components
+  test("deliberate placeholder", () => {});
 });
