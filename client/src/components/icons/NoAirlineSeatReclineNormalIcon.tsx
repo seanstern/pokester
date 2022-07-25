31134022c36ff33e965482b9395a7516765d3e87
seanstern <sean.stern@outlook.com>
2022-07-24 23:38:28 -0400

    Add NoAirlineSeatReclineNormalIcon

---
+++ client/src/components/icons/NoAirlineSeatReclineNormalIcon.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { createSvgIcon } from "@mui/material/utils";
+
+const NoAirlineSeatReclineNormalIcon = createSvgIcon(
+  <>
+    <path d="M7.59 5.41c-.78-.78-.78-2.05 0-2.83.78-.78 2.05-.78 2.83 0 .78.78.78 2.05 0 2.83-.79.79-2.05.79-2.83 0zM6 16V7H4v9c0 2.76 2.24 5 5 5h6v-2H9c-1.66 0-3-1.34-3-3zm14 4.07L14.93 15H11.5v-3.68c1.4 1.15 3.6 2.16 5.5 2.16v-2.16c-1.66.02-3.61-.87-4.67-2.04l-1.4-1.55c-.19-.21-.43-.38-.69-.5-.29-.14-.62-.23-.96-.23h-.03C8.01 7 7 8.01 7 9.25V15c0 1.66 1.34 3 3 3h5.07l3.5 3.5L20 20.07z"></path>
+    <path d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10 10-4.5 10-10S17.5 2 12 2zM4 12c0-4.4 3.6-8 8-8 1.8 0 3.5.6 4.9 1.7L5.7 16.9C4.6 15.5 4 13.8 4 12zm8 8c-1.8 0-3.5-.6-4.9-1.7L18.3 7.1C19.4 8.5 20 10.2 20 12c0 4.4-3.6 8-8 8z"></path>
+  </>,
+  "NoAirlineSeatReclineNormal"
+);
+
+export default NoAirlineSeatReclineNormalIcon;
