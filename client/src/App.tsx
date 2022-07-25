0306efaa83de2af09f2dca362525c4c62582d051
Sean Stern <sean.stern@outlook.com>
2022-07-25 13:38:02 -0400

    New theme (#39)
    * Update App.tsx
    
    New theme lives at root
    
    * Update App.tsx
    
    Because imports need organizing
    
    * Update LoadingProgress
    
    To use secondary color

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,45 +1,61 @@
-import React, { FC } from "react";
+import { deepPurple, orange } from "@mui/material/colors";
+import Container from "@mui/material/Container";
+import CssBaseline from "@mui/material/CssBaseline";
+import createTheme from "@mui/material/styles/createTheme";
+import ThemeProvider from "@mui/material/styles/ThemeProvider";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import React, { FC, useMemo } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import {
   BrowserRouter as Router,
-  Switch,
-  Route,
   Redirect,
-  Link,
+  Route,
+  Switch,
 } from "react-router-dom";
-import Room from "./Room";
 import RoomsList from "./components/rooms-list";
 import CreateRoom from "./CreateRoom";
-import Container from "@mui/material/Container";
+import Room from "./Room";
 
 const qc = new QueryClient({
   defaultOptions: { queries: { refetchOnWindowFocus: false } },
 });
 
-const App: FC = () => (
-  <QueryClientProvider client={qc}>
-    <Router>
-      <Container>
-        <Link to="/create">Create a Room</Link>
-        {" or "}
-        <Link to="/rooms">Browse Rooms</Link>{" "}
-        <Link to={"/rooms?canSit=true"}>(To Join)</Link>{" "}
-        <Link to={"/rooms?isSeated=true"}>(You're Already In)</Link>
-        <Switch>
-          <Route strict path="/rooms/:roomId">
-            <Room />
-          </Route>
-          <Route strict exact path="/rooms">
-            <RoomsList />
-          </Route>
-          <Route strict path="/create">
-            <CreateRoom />
-          </Route>
-          <Redirect to="/rooms" />
-        </Switch>
-      </Container>
-    </Router>
-  </QueryClientProvider>
-);
+const App: FC = () => {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? "dark" : "light",
+          primary: prefersDarkMode ? orange : deepPurple,
+          secondary: prefersDarkMode ? deepPurple : orange,
+        },
+      }),
+    [prefersDarkMode]
+  );
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <QueryClientProvider client={qc}>
+        <Router>
+          <Container>
+            <Switch>
+              <Route strict path="/rooms/:roomId">
+                <Room />
+              </Route>
+              <Route strict exact path="/rooms">
+                <RoomsList />
+              </Route>
+              <Route strict path="/create">
+                <CreateRoom />
+              </Route>
+              <Redirect to="/rooms" />
+            </Switch>
+          </Container>
+        </Router>
+      </QueryClientProvider>
+    </ThemeProvider>
+  );
+};
 
 export default App;
