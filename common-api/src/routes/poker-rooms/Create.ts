0eaae4f199d736231dbb93dcbf8b65580a18c7f0
seanstern <sean.stern@outlook.com>
2022-07-31 21:30:21 -0400

    Update Create to include reqBodySchema
    Because client and server should be able to use common yup schema for
    form and request validation, respectively

--- common-api/src/routes/poker-rooms/Create.ts
+++ common-api/src/routes/poker-rooms/Create.ts
@@ -1,7 +1,66 @@
 import { Table } from "@chevtek/poker-engine";
+import { number, object, SchemaOf, string } from "yup";
 
 export type ReqBody = {
   name: string;
-} & Partial<Pick<Table, "buyIn" | "smallBlind" | "bigBlind">>;
+} & Pick<Table, "buyIn" | "smallBlind" | "bigBlind">;
+
+const userFriendlyNumberTypeError = "${path} must be a number";
+
+export const smallBlindLabel = "small blind";
+export const bigBlindLabel = "big blind";
+export const buyInLabel = "buy-in";
+export const nameLabel = "name";
+
+export const defaultSmallBlind = 0.5;
+export const defaultBigBlindToSmallBlindRatio = 2;
+export const defaultBuyInToBigBlindRatio = 20;
+
+export const reqBodySchema: SchemaOf<ReqBody> = object({
+  name: string()
+    .label(nameLabel)
+    .required()
+    .min(3)
+    .max(40)
+    .matches(
+      /^[A-Za-z0-9 ]*$/,
+      "${path} must contain only alphanumeric chars and spaces"
+    )
+    .matches(
+      /(.*[A-Za-z0-9]){3}/,
+      "${path} must contain at least 3 alphanumeric chars"
+    ),
+  smallBlind: number()
+    .typeError(userFriendlyNumberTypeError)
+    .label(smallBlindLabel)
+    .required()
+    .positive()
+    .default(defaultSmallBlind),
+  bigBlind: number()
+    .label(bigBlindLabel)
+    .typeError(userFriendlyNumberTypeError)
+    .required()
+    .positive()
+    .when(
+      "smallBlind",
+      (smallBlind: number, schema: ReturnType<typeof number>) =>
+        schema
+          .moreThan(
+            smallBlind,
+            `\${path} must be greater than ${smallBlindLabel}`
+          )
+          .default(smallBlind * defaultBigBlindToSmallBlindRatio)
+    ),
+  buyIn: number()
+    .label(buyInLabel)
+    .typeError(userFriendlyNumberTypeError)
+    .required()
+    .positive()
+    .when("bigBlind", (bigBlind: number, schema: ReturnType<typeof number>) =>
+      schema
+        .moreThan(bigBlind, `\${path} must be greater than ${bigBlindLabel}`)
+        .default(bigBlind * defaultBuyInToBigBlindRatio)
+    ),
+});
 
 export type ResBody = string;
