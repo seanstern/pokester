99b126e29e94269a4ef7e3cf143ac9aa07a7b651
seanstern <sean.stern@outlook.com>
2022-08-10 12:20:26 -0400

    Update AppBar
    To receive title as prop without resorting to useNavTitle. Page title value
    will be determined via a PageTitleProvider and associated hooks that
    allow for overriding title in child components and fallback to navTitle
    otherwise. This simplifies testing and separates concerns.

--- client/src/components/page-frame/app-bar/AppBar.test.tsx
+++ client/src/components/page-frame/app-bar/AppBar.test.tsx
@@ -1,57 +1,71 @@
+import { createTheme } from "@mui/material/styles";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { MemoryRouter } from "react-router-dom";
-import { flatNavConfig } from "../navigation";
-import AppBar, { logoAlt, menuButtonLabel } from "./AppBar";
+import mediaQuery from "css-mediaquery";
+import AppBar, {
+  defaultSiteName,
+  defaultTitleSeparator,
+  logoAlt,
+  menuButtonLabel,
+  showSiteNameBreakPoint,
+} from "./AppBar";
 
-describe("renders with navConfig based title", () => {
-  test.each(flatNavConfig)("$path renders $title", async ({ path, title }) => {
-    const user = userEvent.setup();
+const createMatchMedia = (width: number) => (query: string) =>
+  ({
+    matches: mediaQuery.match(query, { width }),
+    addListener: () => {},
+    removeListener: () => {},
+  } as unknown as ReturnType<typeof window.matchMedia>);
 
-    const onMenuClick = jest.fn(() => {});
+const defaultTheme = createTheme();
 
-    render(
-      <MemoryRouter initialEntries={[path]}>
-        <AppBar onMenuClick={onMenuClick} />
-      </MemoryRouter>
-    );
+const showSiteNameWidth =
+  defaultTheme.breakpoints.values[showSiteNameBreakPoint];
 
-    screen.getByRole("heading", { name: title, level: 1 });
+test(`renders title (include site name) and clickable menu button at or above "${showSiteNameBreakPoint}" breakpoint`, async () => {
+  window.matchMedia = createMatchMedia(showSiteNameWidth);
 
-    screen.getByRole("img", { name: logoAlt });
+  const user = userEvent.setup();
 
-    const menuButton = screen.getByRole("button", { name: menuButtonLabel });
+  const title = "Some Title";
+  const onMenuClick = jest.fn(() => {});
 
-    await user.click(menuButton);
+  render(<AppBar onMenuClick={onMenuClick} title={title} />);
 
-    expect(onMenuClick).toHaveBeenCalledTimes(1);
+  screen.getByRole("heading", {
+    name: `${defaultSiteName}${defaultTitleSeparator}${title}`,
+    level: 1,
   });
+
+  screen.getByRole("img", { name: logoAlt });
+
+  const menuButton = screen.getByRole("button", { name: menuButtonLabel });
+
+  await user.click(menuButton);
+
+  expect(onMenuClick).toHaveBeenCalledTimes(1);
 });
 
-describe("renders with overridden title", () => {
-  const overriddentTitle = "Overridden Title";
-  test.each(flatNavConfig)(
-    `$path renders ${overriddentTitle}`,
-    async ({ path }) => {
-      const user = userEvent.setup();
+test(`renders title (excluding site name) and clickable menu button below "${showSiteNameBreakPoint}" breakpoint`, async () => {
+  window.matchMedia = createMatchMedia(showSiteNameWidth - 1);
 
-      const onMenuClick = jest.fn(() => {});
+  const user = userEvent.setup();
 
-      render(
-        <MemoryRouter initialEntries={[path]}>
-          <AppBar onMenuClick={onMenuClick} title={overriddentTitle} />
-        </MemoryRouter>
-      );
+  const title = "Some Title";
+  const onMenuClick = jest.fn(() => {});
 
-      screen.getByRole("heading", { name: overriddentTitle, level: 1 });
+  render(<AppBar onMenuClick={onMenuClick} title={title} />);
 
-      screen.getByRole("img", { name: logoAlt });
+  screen.getByRole("heading", {
+    name: `${defaultTitleSeparator}${title}`,
+    level: 1,
+  });
 
-      const menuButton = screen.getByRole("button", { name: menuButtonLabel });
+  screen.getByRole("img", { name: logoAlt });
 
-      await user.click(menuButton);
+  const menuButton = screen.getByRole("button", { name: menuButtonLabel });
 
-      expect(onMenuClick).toHaveBeenCalledTimes(1);
-    }
-  );
+  await user.click(menuButton);
+
+  expect(onMenuClick).toHaveBeenCalledTimes(1);
 });
--- client/src/components/page-frame/app-bar/AppBar.tsx
+++ client/src/components/page-frame/app-bar/AppBar.tsx
@@ -1,48 +1,67 @@
 import MenuIcon from "@mui/icons-material/Menu";
 import MuiAppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
+import Fade from "@mui/material/Fade";
 import IconButton from "@mui/material/IconButton";
+import { useTheme } from "@mui/material/styles";
 import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import useMediaQuery from "@mui/material/useMediaQuery";
 import React, { FC } from "react";
-import { NavTitle } from "../navigation";
+import { SwitchTransition } from "react-transition-group";
 import pokesterImage from "./pokester.png";
 
 export const menuButtonLabel = "Menu";
 export const logoAlt = "Joker Card";
+export const defaultSiteName = "Pokester ";
+export const defaultTitleSeparator = "/ ";
+export const showSiteNameBreakPoint = "sm";
 
-type AppBarProps = { title?: string; onMenuClick: () => void };
+type AppBarProps = { title: string; onMenuClick: () => void };
 /**
- * Given props, returns an AppBar which displays the navConfig based title and
- * a responsive menu button.
+ * Given props, returns an AppBar which displays the title and a responsive
+ * menu button.
  *
  * @param props
- * @param props.title An optional title that replaces the navConfig based title
+ * @param props.title a title
  * @param props.onMenuClick A callback for when the mobile menu button is
  *   clicked
- * @returns an AppBar which displays the navConfig based title and a responsive
- *   menu button.
+ * @returns an AppBar which displays a title and a responsive menu button.
  */
-const AppBar: FC<AppBarProps> = ({ title, onMenuClick }) => (
-  <MuiAppBar
-    position="fixed"
-    sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
-  >
-    <Toolbar>
-      <IconButton
-        color="inherit"
-        aria-label={menuButtonLabel}
-        edge="start"
-        onClick={onMenuClick}
-        sx={{ mr: 2, display: { md: "none" } }}
-      >
-        <MenuIcon />
-      </IconButton>
-      <Box mr={1}>
-        <img src={pokesterImage} alt={logoAlt} />
-      </Box>
-      <NavTitle overrideTitle={title} />
-    </Toolbar>
-  </MuiAppBar>
-);
+const AppBar: FC<AppBarProps> = ({ title, onMenuClick }) => {
+  const theme = useTheme();
+  const showSiteName = useMediaQuery(
+    theme.breakpoints.up(showSiteNameBreakPoint)
+  );
+  return (
+    <MuiAppBar
+      position="fixed"
+      sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+    >
+      <Toolbar>
+        <IconButton
+          color="inherit"
+          aria-label={menuButtonLabel}
+          edge="start"
+          onClick={onMenuClick}
+          sx={{ mr: 2, display: { md: "none" } }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Box mr={{ xs: 0.5, sm: 1 }}>
+          <img src={pokesterImage} alt={logoAlt} />
+        </Box>
+        <Typography variant="h6" component="h1" noWrap>
+          {`${showSiteName ? defaultSiteName : ""}${defaultTitleSeparator}`}
+          <SwitchTransition>
+            <Fade key={title}>
+              <span>{title}</span>
+            </Fade>
+          </SwitchTransition>
+        </Typography>
+      </Toolbar>
+    </MuiAppBar>
+  );
+};
 
 export default AppBar;
