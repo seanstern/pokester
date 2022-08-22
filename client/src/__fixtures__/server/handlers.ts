ea305cd1c26e627049266461aeb77c44b53493e2
seanstern <sean.stern@outlook.com>
2022-08-21 21:40:52 -0400

    Update @pokester/client
    To use @pokester/common-api shorter namespace

--- client/src/__fixtures__/server/handlers.ts
+++ client/src/__fixtures__/server/handlers.ts
@@ -1,13 +1,13 @@
 import { rest, PathParams } from "msw";
-import { Routes } from "@pokester/common-api";
+import { PokerRooms } from "@pokester/common-api";
 
 const roomId = "62ccf81d289c60d0176a7bc3";
 
 const handlers = [
   rest.post<
-    Routes.PokerRooms.Create.ReqBody,
+    PokerRooms.Create.ReqBody,
     PathParams<string>,
-    Routes.PokerRooms.Create.ResBody
+    PokerRooms.Create.ResBody
   >("/api/rooms", async (req, res, ctx) => {
     return res(ctx.status(201), ctx.json(roomId));
   }),
