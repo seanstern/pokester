b1cec61f77eed40574d2350ae8f33e7b4496ac0a
seanstern <sean.stern@outlook.com>
2022-09-09 15:07:13 -0400

    Add Pots

---
+++ client/src/components/room/pots/Pots.test.tsx
@@ -0,0 +1,50 @@
+import { PokerRooms } from "@pokester/common-api";
+import { render, screen, within } from "@testing-library/react";
+import Pots, { potsRegionLabel, sidePotsRegionLabel } from "./Pots";
+import toCurrencyFormat from "../toCurrencyFormat";
+
+describe("renders main pot only", () => {
+  test.each(Object.values(PokerRooms.Get.Fixtures.Pot))(
+    "$description",
+    ({ create }) => {
+      const pot = create();
+
+      render(<Pots pots={[pot]} />);
+
+      const potsRegion = screen.getByRole("region", { name: potsRegionLabel });
+      within(potsRegion).getByRole("heading", {
+        level: 2,
+        name: potsRegionLabel,
+      });
+      within(potsRegion).getByText(toCurrencyFormat(pot.amount));
+
+      expect(
+        within(potsRegion).queryByRole("region", { name: sidePotsRegionLabel })
+      ).toBeNull();
+    }
+  );
+});
+
+describe("renders main pot with side pots", () => {
+  test.each(Object.values(PokerRooms.Get.Fixtures.Pot))(
+    "$description",
+    ({ create }) => {
+      const pot = create();
+
+      render(<Pots pots={[pot, pot, pot]} />);
+
+      const potsRegion = screen.getByRole("region", { name: potsRegionLabel });
+      within(potsRegion).getByRole("heading", {
+        level: 2,
+        name: toCurrencyFormat(pot.amount),
+      });
+
+      const sidePotsRegion = within(potsRegion).getByRole("region", {
+        name: sidePotsRegionLabel,
+      });
+      within(sidePotsRegion).getAllByText(toCurrencyFormat(pot.amount), {
+        exact: false,
+      });
+    }
+  );
+});
---
+++ client/src/components/room/pots/Pots.tsx
@@ -0,0 +1,67 @@
+import Typography from "@mui/material/Typography";
+import { PokerRooms } from "@pokester/common-api";
+import { FC } from "react";
+import toCurrencyFormat from "../toCurrencyFormat";
+import useCurrencyColor from "../useCurrencyColor";
+import Box from "@mui/material/Box";
+
+export const potsRegionLabel = "Pot";
+const sidePotsLabelId = "side-pots-id";
+export const sidePotsRegionLabel = "Side Pot(s)";
+
+export type PotsProps = {
+  pots: PokerRooms.Get.Pot[];
+};
+/**
+ * Given props, returns the pots at the table.
+ *
+ * @param props
+ * @param props.pots the pots
+ * @returns pots at the table
+ */
+const Pots: FC<PotsProps> = ({ pots }) => {
+  const currencyColor = useCurrencyColor();
+  const [mainPots, ...sidePots] = [...pots].reverse();
+  const hasSidePots = sidePots.length > 0;
+  return (
+    <Box
+      component="section"
+      textAlign="center"
+      minHeight={82}
+      aria-label={potsRegionLabel}
+    >
+      {!hasSidePots && (
+        <Typography component="h2" variant="caption">
+          {potsRegionLabel}
+        </Typography>
+      )}
+      <Typography
+        component={!hasSidePots ? "p" : "h2"}
+        variant={!hasSidePots ? "h5" : "body1"}
+        fontWeight="bold"
+        color={currencyColor}
+      >
+        {toCurrencyFormat(mainPots.amount)}
+      </Typography>
+      {hasSidePots && (
+        <Box component="section" aria-labelledby={sidePotsLabelId}>
+          <Typography variant="caption" component="h3" id={sidePotsLabelId}>
+            {sidePotsRegionLabel}
+          </Typography>
+          <Typography
+            component="p"
+            variant="body2"
+            noWrap
+            color={currencyColor}
+          >
+            {sidePots.map((sp) => toCurrencyFormat(sp.amount)).join(", ") || (
+              <>&nbsp;</>
+            )}
+          </Typography>
+        </Box>
+      )}
+    </Box>
+  );
+};
+
+export default Pots;
