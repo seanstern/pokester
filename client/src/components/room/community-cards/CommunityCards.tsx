992e8d931fdd313d1870b1527a3139624e1624e8
seanstern <sean.stern@outlook.com>
2022-09-09 15:06:47 -0400

    Add CommunityCards

---
+++ client/src/components/room/community-cards/CommunityCards.tsx
@@ -0,0 +1,38 @@
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { PokerRooms } from "@pokester/common-api";
+import { FC } from "react";
+import PlayingCard from "../playing-card";
+import cardToString from "../cardToString";
+
+const communityCardsLabelId = "community-cards-id";
+export const communityCardsRegionLabel = "Community Cards";
+
+export type CommunityCardsProps = {
+  communityCards: PokerRooms.Get.Card[];
+};
+/**
+ * Given props, returns the community cards.
+ *
+ * @param props
+ * @param props.communityCards An array of cards
+ * @returns the community cards
+ */
+const CommunityCards: FC<CommunityCardsProps> = ({ communityCards }) => (
+  <Box
+    component="section"
+    textAlign="center"
+    aria-labelledby={communityCardsLabelId}
+  >
+    <Typography component="h2" variant="caption" id={communityCardsLabelId}>
+      {communityCardsRegionLabel}
+    </Typography>
+    <Box display="flex" justifyContent="center" alignItems="center">
+      {communityCards.map((cc, idx) => (
+        <PlayingCard key={idx} value={cardToString(cc)} color={cc.color} />
+      ))}
+    </Box>
+  </Box>
+);
+
+export default CommunityCards;
