e8e63906d0c6d8aac268215c7badca62d99099c6
seanstern <sean.stern@outlook.com>
2022-09-19 00:57:45 -0400

    Update __fixtures__/server/handlers
    To expose mocked api/rooms/:roomId patch

--- client/src/__fixtures__/server/handlers.ts
+++ client/src/__fixtures__/server/handlers.ts
@@ -1,17 +1,28 @@
 import { PokerRooms } from "@pokester/common-api";
-import { PathParams, rest } from "msw";
+import { rest } from "msw";
+
+export const validRoomIdForPatch = "validRoomIdForPatch";
 
 const handlers = [
-  rest.post<
-    PokerRooms.Create.ReqBody,
-    PathParams<string>,
-    PokerRooms.Create.ResBody
-  >("/api/rooms", async (req, res, ctx) => {
-    return res(
-      ctx.status(201),
-      ctx.json(PokerRooms.Create.Fixtures.ResBody.standard.create())
-    );
-  }),
+  rest.post<PokerRooms.Create.ReqBody, never, PokerRooms.Create.ResBody>(
+    "/api/rooms",
+    async (req, res, ctx) => {
+      return res(
+        ctx.status(201),
+        ctx.json(PokerRooms.Create.Fixtures.ResBody.standard.create())
+      );
+    }
+  ),
+  rest.patch<PokerRooms.Act.ReqBody, { roomId: string }, undefined>(
+    "api/rooms/:roomId",
+    async (req, res, ctx) => {
+      const { roomId } = req.params;
+      if (roomId !== validRoomIdForPatch) {
+        return res(ctx.status(404));
+      }
+      return res(ctx.status(204));
+    }
+  ),
 ];
 
 export default handlers;
