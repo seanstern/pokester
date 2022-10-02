f7d45cb7b50eb3b5c541e369db6c499489dfa734
seanstern <sean.stern@outlook.com>
2022-10-02 17:07:00 -0400

    Update PokerRoomsRouter
    To use new controllers for poker-rooms

--- server/src/routers/PokerRoomsRouter.ts
+++ server/src/routers/PokerRoomsRouter.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
-import { create, get, getAll, act } from "../controllers/PokerRoomsController";
+import { act, create, get, getAll } from "../controllers/poker-rooms";
+import RegistrationExtension from "../middleware/request-extensions/RegistrationExtension";
 
 const PokerRoomsRouter = Router();
+PokerRoomsRouter.use(RegistrationExtension.extend);
 PokerRoomsRouter.route("/").get(getAll).post(create);
 PokerRoomsRouter.route("/:roomId").get(get).patch(act);
 
