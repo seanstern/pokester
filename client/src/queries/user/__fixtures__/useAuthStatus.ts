155a341d954529eeb54efbdb0dc2b150b22a32ac
seanstern <sean.stern@outlook.com>
2022-10-04 15:43:18 -0400

    Add client/queries/user/__fixtures__/useAuthStatus
    To expose query values and associated mswRestHandlers for unit test
    purposes

---
+++ client/src/queries/user/__fixtures__/useAuthStatus.ts
@@ -0,0 +1,68 @@
+import { User } from "@pokester/common-api";
+import { rest, RestHandler } from "msw";
+import { path } from "../config";
+import { AuthStatus, AuthStatusQuery } from "../useAuthStatus";
+
+type UseAuthStatusFixture = Readonly<{
+  query: AuthStatusQuery;
+  mswRestHandler: RestHandler;
+}>;
+
+export const pending: UseAuthStatusFixture = {
+  query: { pending: true },
+  mswRestHandler: rest.get(path, (req, res, ctx) => {
+    return new Promise((res) => {});
+  }),
+};
+
+export const error: UseAuthStatusFixture = {
+  query: {
+    pending: false,
+    error: new Error("Request failed with status code 500"),
+  },
+  mswRestHandler: rest.get(path, (req, res, ctx) => res(ctx.status(500))),
+};
+
+export const unauthenticated: UseAuthStatusFixture = {
+  query: {
+    pending: false,
+    data: AuthStatus.UNAUTHENTICATED,
+  },
+  mswRestHandler: rest.get(path, (req, res, ctx) => res(ctx.status(401))),
+};
+
+export const authenticated: UseAuthStatusFixture = {
+  query: {
+    pending: false,
+    data: AuthStatus.AUTHENTICATED,
+  },
+  mswRestHandler: rest.get(path, (req, res, ctx) => res(ctx.status(403))),
+};
+
+export const authorized: UseAuthStatusFixture = {
+  query: {
+    pending: false,
+    data: AuthStatus.AUTHORIZED,
+  },
+  mswRestHandler: rest.get(path, (req, res, ctx) =>
+    res(
+      ctx.status(200),
+      ctx.json(
+        User.Get.Fixtures.ResBody.unregisteredNoUsernameUnverifiedEmail.create()
+      )
+    )
+  ),
+};
+
+export const registered: UseAuthStatusFixture = {
+  query: {
+    pending: false,
+    data: AuthStatus.REGISTERED,
+  },
+  mswRestHandler: rest.get(path, (req, res, ctx) =>
+    res(
+      ctx.status(200),
+      ctx.json(User.Get.Fixtures.ResBody.fullyRegistered.create())
+    )
+  ),
+};
