4ab16008fa7634fb33315efe9a4da6ca47ee974f
seanstern <sean.stern@outlook.com>
2022-10-07 23:34:39 -0400

    Update useGet
    To fix bug whereby path was being used for queryKey incorrectly

--- client/src/queries/user/useGet.ts
+++ client/src/queries/user/useGet.ts
@@ -1,10 +1,10 @@
 import { User } from "@pokester/common-api";
 import axios, { AxiosError } from "axios";
 import { useQuery } from "react-query";
-import { path } from "./config";
+import { queryKey, path } from "./config";
 
 const useGet = () =>
-  useQuery<User.Get.ResBody, AxiosError<string>>(path, async () => {
+  useQuery<User.Get.ResBody, AxiosError<string>>(queryKey, async () => {
     const { data } = await axios.get<User.Get.ResBody>(path, {
       validateStatus: (status) => status === 200,
     });
