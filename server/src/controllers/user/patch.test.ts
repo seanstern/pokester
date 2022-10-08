1eb63c1f6fa4581ba6adff065bd38ec586b4eb66
seanstern <sean.stern@outlook.com>
2022-10-08 18:52:10 -0400

    Update server/controllers/user/patch
    To reject username changes, non-unique usernames

--- server/src/controllers/user/patch.test.ts
+++ server/src/controllers/user/patch.test.ts
@@ -1,7 +1,13 @@
 import { getMockReq, getMockRes } from "@jest-mock/express";
 import OIDCIdentifierExtension from "../../middleware/request-extensions/OIDCIdentifierExtension";
-import patch, { ReqBody, badRequestMessage } from "./patch";
 import User from "../../models/user";
+import patch, {
+  badRequestMessage,
+  existingUsernameUnchangedFilter,
+  ReqBody,
+  usernameChangeMessage,
+  usernameTakenMessage,
+} from "./patch";
 
 const mockOIDCIdentifier = { iss: "issuer", sub: "subject" };
 jest.spyOn(OIDCIdentifierExtension, "get").mockReturnValue(mockOIDCIdentifier);
@@ -21,7 +27,10 @@ test("succeeds with username field in request body", async () => {
 
   expect(mockUpdateOne).toHaveBeenCalledTimes(1);
   expect(mockUpdateOne).toHaveBeenCalledWith(
-    { oidc: mockOIDCIdentifier },
+    {
+      oidc: mockOIDCIdentifier,
+      ...existingUsernameUnchangedFilter(body.username),
+    },
     body,
     { upsert: true }
   );
@@ -31,18 +40,78 @@ test("succeeds with username field in request body", async () => {
   expect(res.send).toHaveBeenCalledWith();
 });
 
-test("fails with no properties in request vale", async () => {
-  const body: ReqBody = {};
-  const req = getMockReq<Parameters<typeof patch>[0]>({ body });
-  const { res, next } = getMockRes<Parameters<typeof patch>[1]>();
+describe("fails", () => {
+  test("with no reuqest body", async () => {
+    const body: ReqBody = {};
+    const req = getMockReq<Parameters<typeof patch>[0]>({ body });
+    const { res, next } = getMockRes<Parameters<typeof patch>[1]>();
 
-  await patch(req, res, next);
+    await patch(req, res, next);
 
-  expect(mockUpdateOne).not.toHaveBeenCalled();
-  expect(next).not.toHaveBeenCalled();
+    expect(mockUpdateOne).not.toHaveBeenCalled();
+    expect(next).not.toHaveBeenCalled();
 
-  expect(res.status).toHaveBeenCalledTimes(1);
-  expect(res.status).toHaveBeenCalledWith(400);
-  expect(res.send).toHaveBeenCalledTimes(1);
-  expect(res.send).toHaveBeenCalledWith(badRequestMessage);
+    expect(res.status).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.send).toHaveBeenCalledTimes(1);
+    expect(res.send).toHaveBeenCalledWith(badRequestMessage);
+  });
+
+  test("with username that doesn't match pre-existing username in document", async () => {
+    const body: ReqBody = { username: "doesntmatch" };
+    const req = getMockReq<Parameters<typeof patch>[0]>({ body });
+    const { res, next } = getMockRes<Parameters<typeof patch>[1]>();
+
+    mockUpdateOne.mockRejectedValueOnce({
+      code: 11000,
+      keyPattern: { "oidc.iss": 1, "oidc.sub": 1 },
+    });
+
+    await patch(req, res, next);
+
+    expect(next).not.toHaveBeenCalled();
+
+    expect(mockUpdateOne).toHaveBeenCalledTimes(1);
+    expect(mockUpdateOne).toHaveBeenCalledWith(
+      {
+        oidc: mockOIDCIdentifier,
+        ...existingUsernameUnchangedFilter(body.username),
+      },
+      body,
+      { upsert: true }
+    );
+    expect(res.status).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.send).toHaveBeenCalledTimes(1);
+    expect(res.send).toHaveBeenCalledWith(usernameChangeMessage);
+  });
+
+  test("with username that isn't unique", async () => {
+    const body: ReqBody = { username: "matchesanotheruser" };
+    const req = getMockReq<Parameters<typeof patch>[0]>({ body });
+    const { res, next } = getMockRes<Parameters<typeof patch>[1]>();
+
+    mockUpdateOne.mockRejectedValueOnce({
+      code: 11000,
+      keyPattern: { username: 1 },
+    });
+
+    await patch(req, res, next);
+
+    expect(next).not.toHaveBeenCalled();
+
+    expect(mockUpdateOne).toHaveBeenCalledTimes(1);
+    expect(mockUpdateOne).toHaveBeenCalledWith(
+      {
+        oidc: mockOIDCIdentifier,
+        ...existingUsernameUnchangedFilter(body.username),
+      },
+      body,
+      { upsert: true }
+    );
+    expect(res.status).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.send).toHaveBeenCalledTimes(1);
+    expect(res.send).toHaveBeenCalledWith(usernameTakenMessage(body.username));
+  });
 });
--- server/src/controllers/user/patch.ts
+++ server/src/controllers/user/patch.ts
@@ -9,6 +9,27 @@ export { ReqBody };
 export const badRequestMessage =
   "Request body must contain at least one property";
 
+export const usernameTakenMessage = (username?: string) =>
+  `"${username}" has already been taken`;
+
+export const usernameChangeMessage =
+  "Usernames can't be changed after initial selection";
+
+/**
+ * Given an optional username, returns a filter for updateOne that ensures that
+ * a document with a pre-existing username cannot be changed.
+ *
+ * @param username an optional username
+ * @returnsa a filter for updateOne that ensures that a document with a
+ *   pre-exiting username cannot be changed.
+ */
+export const existingUsernameUnchangedFilter = (username?: string) => {
+  if (!username) {
+    return {};
+  }
+  return { $or: [{ username: { $exists: false } }, { username }] };
+};
+
 /**
  * Given an HTTP request to patch a User, an HTTP response, and a callback,
  * attempts to patch the User. Responds with a status of 204 patch is
@@ -35,11 +56,39 @@ const patch: RequestHandler<Record<string, never>, string, ReqBody> = async (
       return;
     }
 
-    await User.updateOne({ oidc }, body, { upsert: true });
-
-    return res.status(204).send();
+    try {
+      await User.updateOne(
+        {
+          oidc,
+          ...existingUsernameUnchangedFilter(body.username),
+        },
+        body,
+        { upsert: true }
+      );
+    } catch (err: any) {
+      if (
+        err.code === 11000 &&
+        err.keyPattern?.username === 1 &&
+        body.username
+      ) {
+        res.status(400).send(usernameTakenMessage(body.username));
+        return;
+      }
+      if (
+        err.code === 11000 &&
+        err.keyPattern["oidc.iss"] === 1 &&
+        err.keyPattern["oidc.sub"] === 1
+      ) {
+        res.status(400).send(usernameChangeMessage);
+        return;
+      }
+      throw err;
+    }
+    res.status(204).send();
+    return;
   } catch (err) {
-    return next(err);
+    next(err);
+    return;
   }
 };
 
