88301990d95be473af862880c6c273b0351c9c43
seanstern <sean.stern@outlook.com>
2022-10-16 16:21:06 -0400

    Add Home

---
+++ client/src/components/home/Home.tsx
@@ -0,0 +1,32 @@
+import Box from "@mui/material/Box";
+import { FC } from "react";
+import ScrollExp from "../utils/ScrollToAnchor";
+import About from "./About";
+import Contact from "./Contact";
+import Welcome from "./Welcome";
+
+const Home: FC = () => (
+  <Box maxWidth="md">
+    <ScrollExp id="welcome">
+      <Welcome />
+    </ScrollExp>
+    <Box
+      bgcolor="secondary.light"
+      color="secondary.contrastText"
+      mt={-3}
+      mb={6}
+      mx={-3}
+      px={3}
+      py={6}
+    >
+      <ScrollExp id="about">
+        <About />
+      </ScrollExp>
+    </Box>
+    <ScrollExp id="contact">
+      <Contact />
+    </ScrollExp>
+  </Box>
+);
+
+export default Home;
