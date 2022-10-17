0c42e93d6ecfe960813388921c5723993e877110
seanstern <sean.stern@outlook.com>
2022-10-17 14:55:18 -0400

    Update client/App
    To use new ThemeProvider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,8 @@
-import CssBaseline from "@mui/material/CssBaseline";
-import createTheme from "@mui/material/styles/createTheme";
-import ThemeProvider from "@mui/material/styles/ThemeProvider";
-import useMediaQuery from "@mui/material/useMediaQuery";
-import { FC, useMemo } from "react";
+import { FC } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter } from "react-router-dom";
 import PageFrame from "./components/page-frame";
+import { ThemeProvider } from "./theme";
 import ScrollToTop from "./components/utils/ScrollToTop";
 import Routes from "./routes";
 
@@ -13,34 +10,17 @@ const qc = new QueryClient({
   defaultOptions: { queries: { refetchOnWindowFocus: false } },
 });
 
-const App: FC = () => {
-  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
-  const theme = useMemo(
-    () =>
-      createTheme({
-        palette: {
-          mode: prefersDarkMode ? "dark" : "light",
-          primary: prefersDarkMode ? { main: "#fdd835" } : { main: "#4527a0" },
-          secondary: prefersDarkMode
-            ? { main: "#4527a0" }
-            : { main: "#fdd835" },
-        },
-      }),
-    [prefersDarkMode]
-  );
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <QueryClientProvider client={qc}>
-        <BrowserRouter>
-          <ScrollToTop />
-          <PageFrame>
-            <Routes />
-          </PageFrame>
-        </BrowserRouter>
-      </QueryClientProvider>
-    </ThemeProvider>
-  );
-};
+const App: FC = () => (
+  <ThemeProvider>
+    <QueryClientProvider client={qc}>
+      <BrowserRouter>
+        <ScrollToTop />
+        <PageFrame>
+          <Routes />
+        </PageFrame>
+      </BrowserRouter>
+    </QueryClientProvider>
+  </ThemeProvider>
+);
 
 export default App;
