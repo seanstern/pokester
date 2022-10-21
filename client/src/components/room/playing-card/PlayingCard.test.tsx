2cdfff82cbd47a8c854d1a424b9cc436700f6344
seanstern <sean.stern@outlook.com>
2022-10-20 21:26:26 -0400

    Update PlayingCard
    To use transitions, render both visible and hidden cards

--- client/src/components/room/playing-card/PlayingCard.test.tsx
+++ client/src/components/room/playing-card/PlayingCard.test.tsx
@@ -1,30 +1,15 @@
-import { PokerRooms } from "@pokester/common-api";
 import { render, screen } from "@testing-library/react";
-import PlayingCard from "./PlayingCard";
+import { deck } from "../../card-utils";
+import PlayingCard, {
+  getVisibleCardLabel,
+  hiddenCardLabel,
+} from "./PlayingCard";
 
-test("renders A♣ with default size", () => {
-  const value = "A♣";
-  const color = PokerRooms.Get.CardColor.BLACK;
-
-  render(<PlayingCard value={value} color={color} />);
-
-  screen.getByText(value);
+test("renders hidden", () => {
+  render(<PlayingCard />);
+  screen.getByRole("generic", { name: hiddenCardLabel });
 });
-
-test("renders 2♥ with sm size", () => {
-  const value = "2♥";
-  const color = PokerRooms.Get.CardColor.RED;
-
-  render(<PlayingCard value={value} color={color} size="sm" />);
-
-  screen.getByText(value);
-});
-
-test("renders T♦ with lg size", () => {
-  const value = "T♦";
-  const color = PokerRooms.Get.CardColor.RED;
-
-  render(<PlayingCard value={value} color={color} size="lg" />);
-
-  screen.getByText(value);
+test.each(deck())(`renders visible $rank$suitChar`, (card) => {
+  render(<PlayingCard visible={card} />);
+  screen.getByRole("generic", { name: getVisibleCardLabel(card) });
 });
