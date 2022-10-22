58996c5ea37f27824ad91e022e7a2f6cfe7fcc8a
seanstern <sean.stern@outlook.com>
2022-10-22 11:19:34 -0400

    Update AllowedUsers
    To use collation

--- server/src/models/allowed-user/AllowedUserSchema.ts
+++ server/src/models/allowed-user/AllowedUserSchema.ts
@@ -8,18 +8,28 @@ export type AllowedUserDoc = {
   email?: string;
 };
 
+const defaultCollation = { locale: "en", strength: 2 };
+
 const AllowedUserSchema = new Schema<AllowedUserDoc>(
   {
     email: {
       type: String,
       required: true,
-      index: true,
-      unique: true,
       immutable: true,
     },
   },
   {
     timestamps: true,
+    collation: defaultCollation,
+  }
+);
+
+AllowedUserSchema.index(
+  { email: 1 },
+  {
+    name: "email_1_collation_en_2",
+    unique: true,
+    collation: defaultCollation,
   }
 );
 
